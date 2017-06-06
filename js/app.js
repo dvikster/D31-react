aa1746e7bf4786c4d9111ff183d29c528226b974
dvikster <dvikster@gmail.com>
2017-06-06 12:36:11 +0300

    added files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
 
+/*
+ ReactDOM.render(
+ React.createElement('h1',null,'Hello'),
+ document.getElementById('root')
+ );*/
